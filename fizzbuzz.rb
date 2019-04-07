adc1f0f9f0742f524433f0893c3bd4f8b360dc3b
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-07 21:37:42 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,14 +2,5 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0 
-    puts "FizzBuzz"
-  elsif int % 3 == 0 && !int % 5 == 0
-    puts "Fizz"
-  elsif !int % 3 == 0 && int % 5 == 0
-    puts "Buzz"
-  else
-    return nil
-  end
-  
+  if int %
 end
