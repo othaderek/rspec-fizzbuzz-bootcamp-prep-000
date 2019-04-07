d1d1aee84a5edbb7a7e36f844cc35bcb0f72fa95
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-07 21:44:33 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,17 +2,4 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(number)
-    divisibleBy3 = (number % 3 == 0)
-    divisibleBy5 = (number % 5 == 0)
-
-    case
-        when divisibleBy3 && divisibleBy5
-            puts "FizzBuzz"
-        when divisibleBy3
-            puts "Fizz"
-        when divisibleBy5
-            puts "Buzz"
-        else 
-            puts number
-    end
 end
