9902bbcd7288dedafe587571b37a50900d33270c
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-07 21:43:35 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,18 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(int)
-  if int % 3 == 0 && !(int % 5 == 0)
-    puts "Fizz"
-    elsif int %
-  end
-end
+def fizzbuzz(number)
+    divisibleBy3 = (number % 3 == 0)
+    divisibleBy5 = (number % 5 == 0)
 
+    case
+        when divisibleBy3 && divisibleBy5
+            puts "FizzBuzz"
+        when divisibleBy3
+            puts "Fizz"
+        when divisibleBy5
+            puts "Buzz"
+        else 
+            puts number
+    end
+end
